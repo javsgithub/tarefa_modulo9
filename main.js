f4f7b8e979433f2b06a89cc02343ecdb8c5392cc
Jefferson Santos <doctorjeffbr@yahoo.com.br>
2024-02-13 17:43:28 -0300

    Finalização do exercício

--- main.js
+++ main.js
@@ -17,9 +17,9 @@ $("document").ready(function() {
     }
 
     $("ul").on("click", "li", function() {
-        $("this").addClass("feito");
-    })
-
+        $(this).toggleClass("feito");
+    })    
+    
     })
 
 
